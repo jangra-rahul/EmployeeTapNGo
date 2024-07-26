8815177228546d8faa723487948ab554a9e3dd6d
jangra-rahul <rahuljangra2122@gmail.com>
2024-07-26 11:45:02 +0530

    modal centered

--- src/app/Dashboard/FamilyMember/ModalComponent.tsx
+++ src/app/Dashboard/FamilyMember/ModalComponent.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Modal, Button, Form, Row } from 'react-bootstrap';
+import React from "react";
+import { Modal, Button, Form, Row } from "react-bootstrap";
 import Apple_wallet from "../../../../public/assets/images/svg/apple-wallet.svg";
 import Samsung_wallet from "../../../../public/assets/images/svg/samsung-wallet.svg";
 import Google_wallet from "../../../../public/assets/images/svg/google_wallet.svg";
@@ -11,35 +11,51 @@ interface ModalComponentProps {
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const ModalComponent: React.FC<ModalComponentProps> = ({ show, handleClose, handleSubmit }) => {
+const ModalComponent: React.FC<ModalComponentProps> = ({
+  show,
+  handleClose,
+  handleSubmit,
+}) => {
   return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton className='border-0 m-0 p-0 pt-4 pe-4'>
-      </Modal.Header >
-      <Modal.Body centered>
-        <Modal.Title className='fs_24 red_ff d-flex justify-content-center dashboard-text-color pb-2 mb-3 mt-3 fw-semibold'>Add Family Member</Modal.Title>
-        <Form onSubmit={handleSubmit} className='mx-3'>
+    <Modal show={show} centered onHide={handleClose}>
+      <Modal.Header
+        closeButton
+        className="border-0 m-0 p-0 pt-4 pe-4"
+      ></Modal.Header>
+      <Modal.Body>
+        <Modal.Title className="fs_24 red_ff d-flex justify-content-center dashboard-text-color pb-2 mb-3 mt-3 fw-semibold">
+          Add Family Member
+        </Modal.Title>
+        <Form onSubmit={handleSubmit} className="mx-3">
           <Form.Group as={Row} className="mb-3" controlId="formCardNumber">
-            <Form.Label className='fs-18 red_ff p-0 m-0 pb-2 text-dark' >
+            <Form.Label className="fs-18 red_ff p-0 m-0 pb-2 text-dark">
               Card Number
             </Form.Label>
-           
-      <Form.Control  className='custom-input py-2' type="text" placeholder="Verify" />
+
+            <div className="position-relative px-0">
+              <Form.Control className=" py-2" type="text" />
+              <div className="position-absolute top-0 end-0">
+                <p className="mt-2 mb-0 pe-4 text-success cursor">Verify</p>
+              </div>
+            </div>
           </Form.Group>
 
           <Form.Group as={Row} className="mb-3" controlId="formNameOnCard">
-            <Form.Label className='fs-18 red_ff p-0 m-0 pb-2 text-dark'>
+            <Form.Label className="fs-18 red_ff p-0 m-0 pb-2 text-dark">
               Name on Card
             </Form.Label>
-            <Form.Control className='py-2' type="text" placeholder="" />
+            <Form.Control className="py-2" type="text" placeholder="" />
           </Form.Group>
 
           <Form.Group as={Row} className="mb-3" controlId="formRelation">
-            <Form.Label className='fs-18 red_ff p-0 m-0 pb-2 text dashboard-text-color'>
+            <Form.Label className="fs-18 red_ff p-0 m-0 pb-2 text dashboard-text-color">
               Relation to You
             </Form.Label>
-            <Form.Select className='py-2 fw-semibold red_ff' aria-label="Select Relation">
-              <option className='fw-Semibold'>Son</option>
+            <Form.Select
+              className="py-2 fw-semibold red_ff"
+              aria-label="Select Relation"
+            >
+              <option className="fw-Semibold">Son</option>
               <option value="spouse">Spouse</option>
               <option value="child">Child</option>
               <option value="parent">Parent</option>
@@ -48,11 +64,15 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ show, handleClose, hand
             </Form.Select>
           </Form.Group>
 
-          <Form.Group as={Row} className="mb-3 d-flex dashboard-text-color" controlId="formTwoWaySync">
+          <Form.Group
+            as={Row}
+            className="mb-3 d-flex dashboard-text-color"
+            controlId="formTwoWaySync"
+          >
             <Form.Label column sm={4}>
               Two way sync <img src={exclamation} alt="" />
             </Form.Label>
-            <div className='d-flex gap-3'>
+            <div className="d-flex gap-3">
               <Form.Check
                 type="radio"
                 label="Yes"
@@ -68,7 +88,11 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ show, handleClose, hand
             </div>
           </Form.Group>
 
-          <Button variant="primary" type="submit" className='w-100 red_ff fs-16 py-3 fw-semibold'>
+          <Button
+            variant="primary"
+            type="submit"
+            className="w-100 red_ff fs-16 py-3 fw-semibold"
+          >
             Submit
           </Button>
         </Form>
@@ -78,4 +102,3 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ show, handleClose, hand
 };
 
 export default ModalComponent;
-
